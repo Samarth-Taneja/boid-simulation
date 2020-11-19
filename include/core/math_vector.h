b451a85dc05b3e69cc9062799982810275756b03
Samarth-Taneja <taneja.samarth.2002@gmail.com>
2020-11-18 23:31:45 -0500

    Implemented constructors and rule of 5 in MathVector.

--- include/core/math_vector.h
+++ include/core/math_vector.h
@@ -1,4 +1,5 @@
-#ifndef IDEAL_GAS_MATH_VECTOR_H
+#ifndef MATH_VECTOR_H
+#define MATH_VECTOR_H
 
 #include <math.h>
 
@@ -8,13 +9,50 @@ class MathVector {
  public:
   double x_ = 0;
   double y_ = 0;
+  double z_ = 0;
 
   MathVector() = default;
-  MathVector(double x, double y) : x_(x), y_(y) {};
+  MathVector(double x, double y, double z) : x_(x), y_(y), z_(z) {};
+
+  //Rule of 5
+  /**
+   * Copy assignment operator. Performs a deep copy.
+   */
+  MathVector &operator=(const MathVector&);
+  /**
+   * Copy constructor. Performs a deep copy.
+   */
+  MathVector(const MathVector&);
+  /**
+   * Move assignment operator.
+   */
+  MathVector& operator=(MathVector&&);
+  /**
+   * Move constructor.
+   */
+  MathVector(MathVector&&) noexcept;
+  /**
+   * Destructor.
+   */
+  ~MathVector();
+
+  /**
+   * @return The magnitude of the vector
+   */
+  double Length();
+
+  /**
+   * Normalizes the vector. Divides each component by the magnitude.
+   */
+  void Normalize();
+
+  /**
+   * @return Vector component corresponding to the provided index.
+   * Vector is 0-indexed.
+   */
+  double &operator[](const size_t index);
 };
 
 }
 
-#define IDEAL_GAS_MATH_VECTOR_H
-
-#endif  // IDEAL_GAS_MATH_VECTOR_H
+#endif  //MATH_VECTOR_H
--- src/core/math_vector.cpp
+++ src/core/math_vector.cpp
@@ -2,4 +2,33 @@
 
 namespace boidsimulation {
 
+//Rule of 5
+MathVector &MathVector::operator=(const MathVector& other_vector) {
+  x_ = other_vector.x_;
+  y_ = other_vector.y_;
+  z_ = other_vector.z_;
+
+  return *this;
+}
+
+MathVector::MathVector(const MathVector& other_vector) {
+  x_ = other_vector.x_;
+  y_ = other_vector.y_;
+  z_ = other_vector.z_;
+}
+
+MathVector &MathVector::operator=(MathVector&& other_vector) {
+  x_ = other_vector.x_; other_vector.x_ = 0;
+  y_ = other_vector.y_; other_vector.y_ = 0;
+  z_ = other_vector.z_; other_vector.z_ = 0;
+
+  return *this;
+}
+
+MathVector::MathVector(MathVector&& other_vector) noexcept {
+  x_ = other_vector.x_; other_vector.x_ = 0;
+  y_ = other_vector.y_; other_vector.y_ = 0;
+  z_ = other_vector.z_; other_vector.z_ = 0;
+}
+
 }
