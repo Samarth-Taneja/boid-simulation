c9231eda612459399ffc4891231da41f52e01189
Samarth-Taneja <taneja.samarth.2002@gmail.com>
2020-11-18 23:37:14 -0500

    Fixed Big 5 implementation.

--- src/core/math_vector.cpp
+++ src/core/math_vector.cpp
@@ -1,34 +1,32 @@
 #include <core/math_vector.h>
 
 namespace boidsimulation {
-
 //Rule of 5
-MathVector &MathVector::operator=(const MathVector& other_vector) {
+MathVector& MathVector::operator=(const MathVector& other_vector) {
   x_ = other_vector.x_;
   y_ = other_vector.y_;
   z_ = other_vector.z_;
 
   return *this;
 }
-
 MathVector::MathVector(const MathVector& other_vector) {
   x_ = other_vector.x_;
   y_ = other_vector.y_;
   z_ = other_vector.z_;
 }
-
-MathVector &MathVector::operator=(MathVector&& other_vector) {
+MathVector& MathVector::operator=(MathVector&& other_vector) {
   x_ = other_vector.x_; other_vector.x_ = 0;
   y_ = other_vector.y_; other_vector.y_ = 0;
   z_ = other_vector.z_; other_vector.z_ = 0;
 
   return *this;
 }
-
 MathVector::MathVector(MathVector&& other_vector) noexcept {
   x_ = other_vector.x_; other_vector.x_ = 0;
   y_ = other_vector.y_; other_vector.y_ = 0;
   z_ = other_vector.z_; other_vector.z_ = 0;
 }
-
+MathVector::~MathVector() {
+  x_ = 0; y_ = 0; z_ = 0;
+}
 }
