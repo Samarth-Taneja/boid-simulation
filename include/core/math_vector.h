0256bd0a82edbae950f6439ecb29bc88527cb0aa
Samarth-Taneja <taneja.samarth.2002@gmail.com>
2020-11-19 02:27:19 -0500

    Implemented MathVector class.

--- include/core/math_vector.h
+++ include/core/math_vector.h
@@ -7,30 +7,31 @@ namespace boidsimulation {
 
 class MathVector {
  public:
-  double x_ = 0;
-  double y_ = 0;
-  double z_ = 0;
+  double x_ = 0, y_ = 0, z_ = 0;
 
   MathVector() = default;
   MathVector(double x, double y, double z) : x_(x), y_(y), z_(z) {};
+
+  void Set(const MathVector& other);
+  void Zero();
 
   //Rule of 5
   /**
    * Copy assignment operator. Performs a deep copy.
    */
-  MathVector &operator=(const MathVector&);
+  MathVector &operator=(const MathVector& other);
   /**
    * Copy constructor. Performs a deep copy.
    */
-  MathVector(const MathVector&);
+  MathVector(const MathVector& other);
   /**
    * Move assignment operator.
    */
-  MathVector& operator=(MathVector&&);
+  MathVector& operator=(MathVector&& other);
   /**
    * Move constructor.
    */
-  MathVector(MathVector&&) noexcept;
+  MathVector(MathVector&& other) noexcept;
   /**
    * Destructor.
    */
@@ -70,6 +71,74 @@ class MathVector {
    * Vector is 0-indexed.
    */
   double &operator[](size_t index);
+  //Equality operator
+  bool operator==(const MathVector& other);
+  //Inequality operator
+  bool operator!=(const MathVector& other);
+
+  //Addition and Subtraction
+  /**
+   * Vector addition and assignment.
+   * Adds components of other to the current MathVector.
+   * @param other MathVector to add to the current MathVector.
+   */
+  MathVector& operator+=(const MathVector& other);
+  /**
+   * Vector subtraction and assignment.
+   * Subtracts components of other from the current MathVector.
+   * @param other MathVector to subtract from the current MathVector.
+   */
+  MathVector& operator-=(const MathVector& other);
+  /**
+   * Vector addition. Adds components of first and second.
+   */
+  friend MathVector& operator+(const MathVector& first, const MathVector& second);
+  /**
+   * Vector subtraction. Subtracts components of second from first.
+   */
+  friend MathVector& operator-(const MathVector& first, const MathVector& second);
+
+  //Scalar multiplication and division
+  /**
+   * Scalar multiplication and assignment.
+   * @param scalar Scalar value to multiply the MathVector by.
+   */
+  MathVector& operator*=(double scalar);
+  /**
+   * Scalar Multiplication.
+   * @param scalar Scalar value to multiply the MathVector by.
+   * @param vector MathVector to be multiplied by the scalar.
+   */
+  friend MathVector& operator*(const double& scalar, const MathVector& vector);
+  /**
+   * Scalar Multiplication.
+   * @param vector MathVector to be multiplied by the scalar.
+   * @param scalar Scalar value to multiply the MathVector by.
+   */
+  friend MathVector& operator*(const MathVector& vector, const double& scalar);
+  //Negates components.
+  MathVector& operator-();
+  /**
+   * Scalar division and assignment.
+   * @param scalar Scalar value to divide the MathVector by.
+   */
+  MathVector& operator/=(double scalar);
+  /**
+   * Scalar Division.
+   * @param vector MathVector to be divided by the scalar.
+   * @param scalar Scalar value to divide the MathVector by.
+   */
+  friend MathVector& operator/(const MathVector& vector, const double& scalar);
+
+  //Dot and Cross product
+  /**
+   * Dot product. Sum of products of components of first and second.
+   */
+  friend double operator*(const MathVector& first, const MathVector& second);
+  /**
+   * Cross product.
+   */
+  friend MathVector& operator%(const MathVector& first, const MathVector& second);
 };
 
 }
--- src/core/math_vector.cpp
+++ src/core/math_vector.cpp
@@ -1,33 +1,33 @@
 #include <core/math_vector.h>
+#include <stdexcept>
 
 namespace boidsimulation {
+void MathVector::Set(const MathVector& other) {
+  x_ = other.x_; y_ = other.y_; z_ = other.z_;
+}
+void MathVector::Zero() {
+  x_ = 0; y_ = 0; z_ = 0;
+}
+
 //Rule of 5
-MathVector& MathVector::operator=(const MathVector& other_vector) {
-  x_ = other_vector.x_;
-  y_ = other_vector.y_;
-  z_ = other_vector.z_;
-
+MathVector& MathVector::operator=(const MathVector& other) {
+  Set(other);
   return *this;
 }
-MathVector::MathVector(const MathVector& other_vector) {
-  x_ = other_vector.x_;
-  y_ = other_vector.y_;
-  z_ = other_vector.z_;
+MathVector::MathVector(const MathVector& other) {
+  Set(other);
 }
-MathVector& MathVector::operator=(MathVector&& other_vector) {
-  x_ = other_vector.x_; other_vector.x_ = 0;
-  y_ = other_vector.y_; other_vector.y_ = 0;
-  z_ = other_vector.z_; other_vector.z_ = 0;
-
+MathVector& MathVector::operator=(MathVector&& other) {
+  Set(other);
+  other.Zero();
   return *this;
 }
-MathVector::MathVector(MathVector&& other_vector) noexcept {
-  x_ = other_vector.x_; other_vector.x_ = 0;
-  y_ = other_vector.y_; other_vector.y_ = 0;
-  z_ = other_vector.z_; other_vector.z_ = 0;
+MathVector::MathVector(MathVector&& other) noexcept {
+  Set(other);
+  other.Zero();
 }
 MathVector::~MathVector() {
-  x_ = 0; y_ = 0; z_ = 0;
+  Zero();
 }
 
 //Vector magnitude related methods
@@ -52,5 +52,80 @@ void MathVector::ChangeDirection(const MathVector& other_vector) {
   double magnitude = Length();
   *this = other_vector;
   ChangeMagnitude(magnitude);
+}
+
+//Vector operations
+double& MathVector::operator[](size_t index) {
+  if(index == 0) {
+    return x_;
+  } else if(index == 1) {
+    return y_;
+  } else if(index == 2) {
+    return z_;
+  } else {
+    throw std::out_of_range("Index out of bounds");
+  }
+}
+//Relational Operations
+bool MathVector::operator==(const MathVector& other) {
+  return (x_ == other.x_ && y_ == other.y_ && z_ == other.z_);
+}
+bool MathVector::operator!=(const MathVector& other) {
+  return !(operator==(other));
+}
+//Vector addition and subtraction
+MathVector& MathVector::operator+=(const MathVector& other) {
+  x_ += other.x_; y_ += other.y_; z_ += other.z_;
+  return *this;
+}
+MathVector& MathVector::operator-=(const MathVector& other) {
+  x_ -= other.x_; y_ -= other.y_; z_ -= other.z_;
+  return *this;
+}
+MathVector& operator+(const MathVector& first, const MathVector& second) {
+  MathVector sum(first.x_ + second.x_, first.y_ + second.y_, first.z_ + second.z_);
+  return sum;
+}
+MathVector& operator-(const MathVector& first, const MathVector& second) {
+  MathVector sum(first.x_ - second.x_, first.y_ - second.y_, first.z_ - second.z_);
+  return sum;
+}
+//Scalar multiplication and division
+MathVector& MathVector::operator*=(double scalar) {
+  x_ *= scalar; y_ *= scalar; z_ *= scalar;
+  return *this;
+}
+MathVector& operator*(const MathVector& vector, const double& scalar) {
+  MathVector product = vector;
+  product *= scalar;
+  return product;
+}
+MathVector& operator*(const double& scalar, const MathVector& vector) {
+  return (operator*(vector, scalar));
+}
+MathVector& MathVector::operator-() {
+  *this *= -1;
+  return *this;
+}
+MathVector& MathVector::operator/=(double scalar) {
+  if(scalar != 0) {
+    x_ /= scalar; y_ /= scalar; z_ /= scalar;
+  }
+  return *this;
+}
+MathVector& operator/(const MathVector& vector, const double& scalar) {
+  MathVector quotient = vector;
+  quotient /= scalar;
+  return quotient;
+}
+//Dot and cross product
+double operator*(const MathVector& first, const MathVector& second) {
+  return ((first.x_ * second.x_) + (first.y_ * second.y_) + (first.z_ * second.z_));
+}
+MathVector& operator%(const MathVector& first, const MathVector& second) {
+  MathVector cross(first.y_*second.z_ - first.z_*second.y_,
+                   first.z_*first.x_ - first.x_*first.z_,
+                   first.x_*second.y_ - first.y_*second.x_);
+  return cross;
 }
 }
